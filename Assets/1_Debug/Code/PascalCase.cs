51d152010613a27ad1f62175a166b4bb7b9a7bec
Euphie_E <espana.elise@gmail.com>
2026-08-24 19:23:48 -0300

    Organizer
    Folder structure and templates for naming convention

---
+++ Assets/1_Debug/Code/PascalCase.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+//namespace DBManager.game
+namespace DBFamily.subFamily
+{
+    public class PascalCase : MonoBehaviour
+    {
+        public static string testVariable = "test";
+        void Awake()
+        {
+            
+        }
+        void OnEnable()
+        {
+            
+        }
+        void OnDisable()
+        {
+            
+        }
+        void Start()
+        {
+            Debug.Log("");
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Src/PascalCase.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+using DBFamily;
+
+//namespace Manager.game
+namespace Family.subFamily
+{   
+    public class PascalCase : MonoBehaviour
+    {
+        string testVariable = DBFamily.subFamily.PascalCase.testVariable;
+        void Awake()
+        {
+            
+        }
+        void OnEnable()
+        {
+            
+        }
+        void OnDisable()
+        {
+            
+        }
+        void Start()
+        {
+            Debug.Log(testVariable);
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
