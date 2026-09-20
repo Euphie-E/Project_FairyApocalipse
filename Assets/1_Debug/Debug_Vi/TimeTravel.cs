b6cab2984ecc5360bf9dc403766fc75604aca779
Vicenzo <Vicenzux@gmail.com>
2026-09-20 17:55:11 -0300

    voltando bobagem do time travel

--- Assets/1_Debug/Debug_Vi/TimeTravel.cs
+++ Assets/1_Debug/Debug_Vi/TimeTravel.cs
@@ -56,12 +56,6 @@ public class TimeTravel : MonoBehaviour
         Physics.IgnoreLayerCollision(playerLayer, futureLayer, false);
         Physics.IgnoreLayerCollision(playerLayer, pastLayer, true);
 
-        Physics.IgnoreLayerCollision(playerLayer, futureLayer, false);
-        Physics.IgnoreLayerCollision(playerLayer, pastLayer, true);
-
-        Physics.IgnoreLayerCollision(playerLayer, futureLayer, false);
-        Physics.IgnoreLayerCollision(playerLayer, pastLayer, true);
-
         // Main Camera nunca renderiza o passado.
         SetMainCameraToFuture();
 
