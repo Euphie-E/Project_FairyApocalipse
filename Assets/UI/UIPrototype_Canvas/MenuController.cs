7edf79a3445621ce1a5f95e871f57c210bdee348
BPulit <brunopulit18@gmail.com>
2026-09-20 22:49:53 -0300

    Fix menu pausa

--- Assets/UI/UIPrototype_Canvas/MenuController.cs
+++ Assets/UI/UIPrototype_Canvas/MenuController.cs
@@ -3,24 +3,39 @@ using UnityEngine;
 public class MenuController : MonoBehaviour
 {
     public GameObject pausePanel;
+    private bool isPaused = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Time.timeScale = 1f;
+        isPaused = false;
         pausePanel.SetActive(false);
         PlayerInput.Instance.AddAction(Pause,6);
     }
 
-    // Update is called once per frame
     public void Pause()
     {
+        // Alterna entre pausado e despausado
+        isPaused = !isPaused;
 
-        Time.timeScale = 0f;
-        pausePanel.SetActive(!pausePanel.activeSelf);
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            pausePanel.SetActive(false);
+        }
     }
 
     public void BackToGame()
     {
-        Time.timeScale = 1;
+        isPaused = false;
+
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
     }
 }
